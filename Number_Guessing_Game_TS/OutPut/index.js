32d3c4f29f376f938f5a4345e70a5fc60a5434be
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-03-29 23:31:28 +0500

    Updated

--- Number_Guessing_Game_TS/OutPut/index.js
+++ Number_Guessing_Game_TS/OutPut/index.js
@@ -1,22 +1,24 @@
 import inquirer from "inquirer";
 console.log('-- Simple Number Guessing Game -- ');
 const RandomValue = Math.floor(Math.random() * 10);
-// console.log('RandomValue', RandomValue)
-const answer = await inquirer.prompt([
-    {
-        type: "input",
-        name: "input_no",
-        message: "Guess The Number"
+async function evaluate() {
+    const answer = await inquirer.prompt([
+        {
+            type: "input",
+            name: "input_no",
+            message: "Guess The Number..."
+        }
+    ]);
+    if (answer.input_no == RandomValue) {
+        console.log('Your are Right Your Guess is Right');
     }
-]);
-// if (answer.input_no == RandomValue) {
-//     console.log('Your are Right Your Guess is Right')
-// }
-// else if (answer.input_no < RandomValue) {
-//     console.log('Your Guess Smaller Number')
-//     getNumber()
-// }
-// else if (answer.input_no > RandomValue) {
-//     console.log('Your Guess Larger Number')
-//     getNumber()
-// }
+    else if (answer.input_no < RandomValue) {
+        console.log('Your Guess Smaller Number');
+        evaluate();
+    }
+    else if (answer.input_no > RandomValue) {
+        console.log('Your Guess Larger Number');
+        evaluate();
+    }
+}
+evaluate();
