e72b53ab318c731fde654df21cc1c0525b89d2bd
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-03-29 23:22:17 +0500

    updates

--- Number_Guessing_Game_TS/OutPut/index.js
+++ Number_Guessing_Game_TS/OutPut/index.js
@@ -1,24 +1,20 @@
-import inquirer from "inquirer"
-console.log('-- Simple Number Guessing Game -- ')
-const RandomValue = Math.floor(Math.random() * 10)
+import inquirer from "inquirer";
+console.log('-- Simple Number Guessing Game -- ');
+const RandomValue = Math.floor(Math.random() * 10);
 // console.log('RandomValue', RandomValue)
 const answer = await inquirer.prompt([
-        {
-            type: "input",
-            name: "input_no",
-            message: "Guess The Number"
-        }
-    ])
-
-
+    {
+        type: "input",
+        name: "input_no",
+        message: "Guess The Number"
+    }
+]);
 // if (answer.input_no == RandomValue) {
 //     console.log('Your are Right Your Guess is Right')
 // }
 // else if (answer.input_no < RandomValue) {
 //     console.log('Your Guess Smaller Number')
 //     getNumber()
-
-
 // }
 // else if (answer.input_no > RandomValue) {
 //     console.log('Your Guess Larger Number')
